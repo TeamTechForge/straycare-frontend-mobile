c3faab732fc682a72a072ff4a5289445375820a0
Amasha <amiyurangi@gmail.com>
2026-04-27 12:39:00 +0530

    cannect database

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,29 @@
-import { Stack } from "expo-router";
+/*import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return <Stack />;
+}*/
+
+/*
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+
+export default function RootLayout() {
+  return (
+    <>
+      <StatusBar hidden />
+      <Stack />
+    </>
+  );
+}*/
+import { Stack } from "expo-router";
+
+export default function RootLayout() {
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+      }}
+    />
+  );
 }
