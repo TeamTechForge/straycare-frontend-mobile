cb539368aa1b7d4014166b2d1c37d8d9407e713b
Amasha <amiyurangi@gmail.com>
2026-07-08 12:23:41 +0530

    changes in ip

--- api/api.js
+++ api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://10.87.129.94:5000",
+  baseURL: "http://192.168.1.6:5000",
   headers: {
     "Content-Type": "application/json",
   },
